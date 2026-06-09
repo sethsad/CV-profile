7f21fdbeb1d41a4fe326334b0046f543d49e4bc6
Vy Piseth <135794492+sethsad@users.noreply.github.com>
2026-06-09 11:50:48 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navList = document.getElementById("nav-list");
+
+    menuToggle.addEventListener("click", () => {
+        navList.classList.toggle("show");
+    });
+});
